2d27373480b186514330438494ef5335c6dbb30f
Derty990 <kluczbart@gmail.com>
2025-04-26 15:28:21 +0200

    commit

---
+++ src/main/java/org/example/arrays/medium/LC54.java
@@ -0,0 +1,53 @@
+package org.example.arrays.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LC54 {
+
+    public List<Integer> spiralOrder(int[][] matrix) {
+
+        List<Integer> result = new ArrayList<>();
+
+        if(matrix==null || matrix[0] == null){
+            return result;
+        }
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int top = 0;
+        int bottom = rows-1;
+        int left = 0;
+        int right = cols-1;
+
+        while(top<= bottom && left <= right){
+            for (int i = left; i <= right ; i++) {
+                result.add(matrix[top][i]);
+            }
+            top++;
+
+            for (int i = top; i <= bottom; i++) {
+                result.add(matrix[i][right]);
+            }
+            right--;
+
+            if(top<=bottom){
+                for (int i = right; i >= left; i--) {
+                    result.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            if(left<=right){
+                for (int i = bottom; i >= top ; i--) {
+                    result.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return result;
+    }
+
+
+}
---
+++ src/main/java/org/example/arrays/medium/LC73.java
@@ -0,0 +1,43 @@
+package org.example.arrays.medium;
+
+public class LC73 {
+    //Input: matrix = [
+    // [0,1,2,0],
+    // [3,4,5,2],
+    // [1,3,1,5]
+    // ]
+
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[] rows = new boolean[m];
+        boolean[] cols = new boolean[n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0){
+                    rows[i] = true;
+                    cols[j] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            if (rows[i]) {
+                for (int j = 0; j < n; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        for (int j = 0; j < n; j++) {
+            if (cols[j]) {
+                for (int i = 0; i < m; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+    }
+
+}
